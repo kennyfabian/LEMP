505ed787c8088c60cd5c4a5983f3714b4dcf328f
Kenny Fabián <kennyfabian@hotmail.com>
2018-11-21 02:02:03 -0400

    Update Yarn_only.sh

--- Yarn_only.sh
+++ Yarn_only.sh
@@ -15,6 +15,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 #Update Ubuntu and Install Yarn
+sudo apt-get install libpng-dev -y
 sudo apt-get update && sudo apt-get install yarn -y
 
 echo ----- NOW YARN PACKAGE MANAGER HAS BEEN INSTALLED, ENJOY ------
